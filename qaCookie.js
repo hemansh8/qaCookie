a3617d6b3d7907df116081e45c1ac553b2c5c31f
Hemansh <hemansh8@gmail.com>
2020-09-26 23:51:36 +0530

    QA cookie value can be given as qaValue parameter

--- qaCookie.js
+++ qaCookie.js
@@ -3,7 +3,12 @@
     var params = new URLSearchParams(window.location.search);
     if (params.has("qaCookie")) {
       var cookieName = params.get("qaCookie");
-      document.cookie = cookieName + "=true; path=/";
+      // cookie value is either true or the value of the qaValue parameter if it exists
+      var cookieValue = "true";
+      if (params.has("qaValue")) {
+        cookieValue = params.get("qaValue");
+      }
+      document.cookie = cookieName + "=" + cookieValue + "; path=/";
     }
   }
   init();
